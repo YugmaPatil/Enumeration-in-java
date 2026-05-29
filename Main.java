5de18b34717ca87a8e53fb7f277abd3d031a042b
YugmaPatil <yugmanemade@gmail.com>
2026-05-29 10:02:14 +0530

    Add files via upload
    enum Color {
        RED("red"), GREEN("green"), BLUE("blue");
            Add enum constants for the defined types.
    
    public String getValue(){
        return value;
    }
    Returns the associated string value.
    
    Color c1 = Color.RED;
    Accessing enum constant.
    
    System.out.println("Red Enum name:"+ c1.name());
    name() returns constant name → RED
    
    System.out.println("Red Enum value :"+ c1.getValue());
    Returns custom value → red
    
    for(Color color : Color.values()){
        System.out.println("Enum Value:" + color.getValue());
    }
    values() returns all enum constants.
    Iterates through all colors.
    
    Red Enum name: RED
    Red Enum value : red
    Enum Value: red
    Enum Value: green
    Enum Value: blue

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Color c1 =Color.RED;
+
+        System.out.println("Red Enum name:"+c1.name());
+        System.out.println("Red Enum value:"+ c1.getValue());
+
+        for (Color color : Color.values()){
+            System.out.println("Enum Value:"+color.getValue());
+        }
+    }
+}
